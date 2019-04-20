03e95936e744e74b2a6aba0a2dde6f4b6d1a6e8f
Jonas Hagstedt <hagstedt@gmail.com>
2019-04-20 19:25:00 +0200

    annotated code

--- v1/src/connections.rs
+++ v1/src/connections.rs
@@ -27,16 +27,23 @@ impl<T: StreamRef> Reactor for Connections<T> {
     fn react(&mut self, reaction: Reaction<Self::Input>) -> Reaction<()> {
         use Reaction::*;
 
-        match self.inner.react(reaction) {
+        match self.inner.inner_react(reaction) {
             Continue => Continue,
             Event(ev) => ev.into(),
             Value(user) => {
+                // All payloads to send
                 let mut payloads = Vec::new();
 
+                // Read data from the connection
                 while let Some(bytes_res) = user.recv() {
                     match bytes_res {
                         Ok(bytes) => {
-                            payloads.push(bytes);
+                            // encode the bytes, which in this 
+                            // instance just appends `\n`
+                            let bytes_mut = BytesMut::from(bytes);
+                            let bytes = user.encode(bytes_mut);
+
+                            payloads.push(bytes)
                         }
 
                         // Connection broke while receiving data
@@ -48,21 +55,17 @@ impl<T: StreamRef> Reactor for Connections<T> {
                     }
                 }
 
-                while let Some(res) = user.write() {
-                    if res.is_err() {
-                        let user_id = user.token();
-                        self.inner.remove(user_id);
-                        return Reaction::Continue;
-                    }
-                }
-
+                // Track all connections that closes
+                // during the write process
                 let mut closed_connections = Vec::new();
 
+                // Iterate over the payloads
                 for bytes in payloads {
                     for (user_id, con) in self.inner.iter_mut() {
-                        let bytes_mut = BytesMut::from(bytes.clone());
-                        let bytes = con.encode(bytes_mut);
-                        con.add_payload(bytes);
+                        // Add the payload to each connection
+                        con.add_payload(bytes.clone());
+
+                        // ... and try to write the data
                         while let Some(res) = con.write() {
                             if res.is_err() {
                                 closed_connections.push(*user_id);
@@ -72,7 +75,7 @@ impl<T: StreamRef> Reactor for Connections<T> {
                     }
                 }
 
-                // Clean up closed connections
+                // Remove closed connections
                 for user_id in closed_connections {
                     self.inner.remove(user_id);
                 }
