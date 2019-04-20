7d152b077e16254b90043080b13f5fd388242148
Jonas Hagstedt <hagstedt@gmail.com>
2019-04-20 17:35:38 +0200

    fixed bug with connections

--- v1/src/connections.rs
+++ v1/src/connections.rs
@@ -25,54 +25,60 @@ impl<T: StreamRef> Reactor for Connections<T> {
     type Output = ();
 
     fn react(&mut self, reaction: Reaction<Self::Input>) -> Reaction<()> {
-        if let Some(user) = self.inner.react(reaction) {
-            let mut payloads = Vec::new();
+        use Reaction::*;
 
-            while let Some(bytes_res) = user.recv() {
-                match bytes_res {
-                    Ok(bytes) => {
-                        payloads.push(bytes);
+        match self.inner.react(reaction) {
+            Continue => Continue,
+            Event(ev) => ev.into(),
+            Value(user) => {
+                let mut payloads = Vec::new();
+
+                while let Some(bytes_res) = user.recv() {
+                    match bytes_res {
+                        Ok(bytes) => {
+                            payloads.push(bytes);
+                        }
+
+                        // Connection broke while receiving data
+                        Err(()) => {
+                            let user_id = user.token();
+                            self.inner.remove(user_id);
+                            return Reaction::Continue;
+                        }
                     }
+                }
 
-                    // Connection broke while receiving data
-                    Err(()) => {
+                while let Some(res) = user.write() {
+                    if res.is_err() {
                         let user_id = user.token();
                         self.inner.remove(user_id);
                         return Reaction::Continue;
                     }
                 }
-            }
 
-            while let Some(res) = user.write() {
-                if res.is_err() {
-                    let user_id = user.token();
-                    self.inner.remove(user_id);
-                    return Reaction::Continue;
-                }
-            }
+                let mut closed_connections = Vec::new();
 
-            let mut closed_connections = Vec::new();
-
-            for bytes in payloads {
-                for (user_id, con) in self.inner.iter_mut() {
-                    let bytes_mut = BytesMut::from(bytes.clone());
-                    let bytes = con.encode(bytes_mut);
-                    con.add_payload(bytes);
-                    while let Some(res) = con.write() {
-                        if res.is_err() {
-                            closed_connections.push(*user_id);
-                            break
+                for bytes in payloads {
+                    for (user_id, con) in self.inner.iter_mut() {
+                        let bytes_mut = BytesMut::from(bytes.clone());
+                        let bytes = con.encode(bytes_mut);
+                        con.add_payload(bytes);
+                        while let Some(res) = con.write() {
+                            if res.is_err() {
+                                closed_connections.push(*user_id);
+                                break
+                            }
                         }
                     }
                 }
-            }
 
-            // Clean up closed connections
-            for user_id in closed_connections {
-                self.inner.remove(user_id);
+                // Clean up closed connections
+                for user_id in closed_connections {
+                    self.inner.remove(user_id);
+                }
+
+                Reaction::Continue
             }
         }
-
-        Reaction::Continue
     }
 }
